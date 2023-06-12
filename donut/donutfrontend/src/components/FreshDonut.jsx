2a607c8c58dcbcb00f89e110fdfaac57bbef51c2
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-06-12 17:41:29 -0600

    Debugged framing

--- donut/donutfrontend/src/components/FreshDonut.jsx
+++ donut/donutfrontend/src/components/FreshDonut.jsx
@@ -50,22 +50,22 @@ in(B),t=c*h*g-f*        e;int x=40+30*D*
 // Refactored to run in react.
 function spin() {
     var pretag = document.getElementById('ascii');
-
     var tmr1 = undefined;
     var A=1, B=1;
     var i;
 
-    // This is copied, pasted, reformatted, and ported directly from my original
-    // donut.c code
     var asciiframe=function() {
         var b=[];
         var z=[];
+        var width = 80
+        var height = 35
+
         A += 0.07;
         B += 0.03;
         var cA=Math.cos(A), sA=Math.sin(A),
             cB=Math.cos(B), sB=Math.sin(B);
-        for(var k=0;k<1760;k++) {
-            b[k]=k%80 === 79 ? "\n" : " ";
+        for(var k=0;k<width*height;k++) {
+            b[k]=k%width === width-1 ? "\n" : " ";
             z[k]=0;
         }
         for(var j=0;j<6.28;j+=0.07) { // j <=> theta
@@ -76,11 +76,11 @@ function spin() {
                 D=1/(sp*h*sA+st*cA+5), // this is 1/z
                 t=sp*h*cA-st*sA; // this is a clever factoring of some of the terms in x' and y'
 
-            var x=0|(40+30*D*(cp*h*cB-t*sB)),
-                y=0|(12+15*D*(cp*h*sB+t*cB)),
+            var x=0|((width/2)+30*D*(cp*h*cB-t*sB)),
+                y=0|((height/2)+15*D*(cp*h*sB+t*cB)),
                 o=x+80*y,
                 N=0|(8*((st*sA-sp*ct*cA)*cB-sp*ct*sA-st*cA-cp*ct*sB));
-            if(y<22 && y>=0 && x>=0 && x<79 && D>z[o])
+            if(y<height && x>=0 && x<width && D>z[o])
             {
                 z[o]=D;
                 b[o]=".,-~:;=!*#$@"[N>0?N:0];
@@ -88,7 +88,7 @@ function spin() {
             }
         }
         pretag.innerHTML = b.join("");
-    };
+    }
 
     if(tmr1 === undefined) {
         tmr1 = setInterval(asciiframe, 51);
